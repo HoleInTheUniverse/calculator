1f6978401c3a4dab8664f6f9cc15d6f7a2044116
HoleInTheUniverse <43988974+HoleInTheUniverse@users.noreply.github.com>
2025-05-25 11:44:12 +0300

    Add basic calculator functions

---
+++ calculator.js
@@ -0,0 +1,36 @@
+let firstOperand = 0;
+let secondOperand = 0;
+let operator = '';
+
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+function operate (firstOperand, secondOperand, operator) {
+    switch (operator) {
+        case 'sum':
+            add(firstOperand, secondOperand);
+            break;
+        case 'subtraction':
+            subtract(firstOperand, secondOperand);
+            break;
+        case 'multiplication':
+            multiply(firstOperand, secondOperand);
+            break;
+        case 'division':
+            divide(firstOperand, secondOperand);
+            break;
+    }
+}
